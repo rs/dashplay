dfb985151401a44bd36c91faa697a687cccefcc7
Olivier Poitrey <rs@dailymotion.com>
2015-08-29 02:18:25 -0700

    Add a landpage + fix some bugs

--- api.go
+++ api.go
@@ -1,15 +1,16 @@
-package main
+package api
 
 import (
 	"encoding/base64"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"time"
+
+	"appengine"
+	"appengine/urlfetch"
 )
 
-func main() {
+func init() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		path := base64.URLEncoding.EncodeToString([]byte(req.URL.Path[1:]))
 		switch req.Method {
@@ -24,16 +25,17 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		client := &http.Client{
-			Timeout: 2 * time.Second,
-		}
+		c := appengine.NewContext(req)
+		client := urlfetch.Client(c)
 		res, err := client.Do(r)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		body, err := ioutil.ReadAll(res.Body)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		for name, values := range res.Header {
 			w.Header().Set(name, values[0])
@@ -41,5 +43,4 @@ func main() {
 		w.WriteHeader(res.StatusCode)
 		w.Write(body)
 	})
-	log.Fatal(http.ListenAndServe(":8081", nil))
 }
