26a50b53231123fb2ef8f301562d7d3daf3e91e4
Olivier Poitrey <rs@dailymotion.com>
2015-08-30 00:38:21 -0700

    Fix a typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-    fs.readFile(__dirname + '/index.html', function(err, data) {
+    fs.readFile(__dirname + '/dashplay.html', function(err, data) {
         res.writeHead(200);
         res.end(data);
     });
